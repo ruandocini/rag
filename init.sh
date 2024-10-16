363bed4f255baf124a826c4642520e37e0e5f1dd
ruandocini <ruandocini@gmail.com>
2024-10-15 22:30:25 -0300

    improved init and added specific versions to the project

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+conda create -n rag-system python=3.8 -y
+source activate base
+conda init
+conda activate rag-system
+pip install -r requirements.txt
 export $(cat .env | xargs)
 cd db
 docker-compose build
